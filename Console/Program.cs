b951fcc8bce3379d8e64b444c1fb7759e1f92818
yemuy <bli24@huskers.unl.edu>
2023-11-03 23:03:12 -0500

    readme.change

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,7 +13,7 @@ namespace GithubActionsLab
                 try
                 {
                     Func<string, string, double> operation = null;
-                    Console.WriteLine("1) (x+y)");
+                    Co"1) (x+y)");
                     Console.WriteLine("2) Subtract (x-y)");
                     Console.WriteLine("3) Multiply (x*y)");
                     Console.WriteLine("4) Divide (x/y)");
